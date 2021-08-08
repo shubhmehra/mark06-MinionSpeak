3cd971eec68dd9a490d79ea205d1e6ab78ac49d3
Shubham Mehra <mehrashubham18@gmail.com>
2021-08-08 12:19:10 +0530

    Bug fix

--- app.js
+++ app.js
@@ -5,26 +5,22 @@ var errorDiv = document.querySelector("#errorMessage");
 
 var serverUrl = "https://api.funtranslations.com/translate/minion.json";
 
-// formatting url
 function getTranslationUrl(text) {
   return serverUrl + "?" + "text=" + text;
 }
 
-// error handling function
 function errorHandler(error) {
   console.log(" error occured", error);
   errorDiv.innerText =
     "We are sorry. You can only use this for 5 times per hour. Try again later.";
 }
 
-// click event
 btnTranslate.addEventListener("click", clickEventHandler());
 
-// handling click event
 function clickEventHandler() {
   return function clickEventHandler() {
     var inputText = txtInput.value;
-
+    console.log(getTranslationUrl(inputText));
     fetch(getTranslationUrl(inputText))
       .then((response) => response.json())
       .then((json) => (outputDiv.innerText = json.contents.translated))
